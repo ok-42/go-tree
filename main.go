cf7a145462a1d679560b79e1ad5658157ab13b16
Эрдни Окунов <61590286+ok-42@users.noreply.github.com>
2023-12-18 02:52:24 +0300

    Add the first version

---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// https://stackoverflow.com/a/10485970
+func contains[anyType int | string](s []anyType, e anyType) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
+
+var ignorePaths = []string{
+	"__pycache__",
+	".git",
+	".idea",
+	".ipynb_checkpoints",
+	"venv",
+}
+
+func read(path string, level int) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, entry := range entries {
+		var name string = entry.Name()
+		var indent string = strings.Repeat(" ", level)
+		fmt.Println(indent, "\u251C\u2500\u2500", name)
+		if entry.Type().IsDir() && !contains(ignorePaths, name) {
+			var newPath string
+			if path == "." {
+				newPath = name
+			} else {
+				newPath = path + name
+			}
+			read(newPath, level+4)
+		}
+		// fmt.Println(indent, "\u2502")
+	}
+}
+
+func main() {
+	var path string = os.Args[1]
+	read(path, 0)
+}
